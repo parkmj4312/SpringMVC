478bb790c0e21df170439e3ad6f8682a4168e4b3
parker4942 <parkmj4312@gmail.com>
2023-02-27 17:28:16 +0900

    MVC 커밋

---
+++ src/main/java/com/springmvc/springmvc/Star.java
@@ -0,0 +1,13 @@
+package com.springmvc.springmvc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Star {
+    String name;
+    int age;
+}
